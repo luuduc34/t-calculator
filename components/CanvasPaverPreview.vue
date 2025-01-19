1539d3e03b29343e86f86387e2fde2d0fb9909b6
Duc Luu <luuduc34@hotmail.com>
2025-01-19 01:14:55 +0100

    adjust preview size

--- components/CanvasPaverPreview.vue
+++ components/CanvasPaverPreview.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="containerRef" style="width: 100%; height: 400px;">
+    <div ref="containerRef" style="width: 100%; height: 100%;">
         <canvas ref="canvasRef" style="display: block; width: 100%; height: 100%;"></canvas>
     </div>
 </template>
